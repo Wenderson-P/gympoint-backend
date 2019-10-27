23d5b01cfd7d55f6a37f992b6b0c1b0286bbad51
Wenderson <wps713@gmail.com>
2019-10-26 22:44:11 -0300

    Criando migration da tabela de estudantes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'gobarber',
+  database: 'gympoint',
   define: {
     timestamps: true,
     underscored: true,
