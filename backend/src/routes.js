223c5e4d8fb67ee3c57e096e735511af6c15fa48
Wenderson <wps713@gmail.com>
2019-10-26 23:21:13 -0300

    Carregando model de usuario

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import Student from './app/models/Student';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello beatiful world' });
+});
+
+routes.get('/create', async (req, res) => {
+  const student = await Student.create({
+    name: 'Wenderson',
+    email: 'wp@gmail.com',
+    password_hash: '1223',
+    age: '22',
+    weight: '70.50',
+    height: '1.80',
+  });
+  return res.json(student);
 });
 
 export default routes;
