49142364a5ca3187231e5b9895bcabd12fb47928
Wenderson <wps713@gmail.com>
2019-10-28 12:05:05 -0300

    Finalizando middleware de autenticação de usuário

--- backend/src/app/middlewares/auth.js
+++ backend/src/app/middlewares/auth.js
@@ -1,5 +1,20 @@
-export default (req, res, next) => {
+import { promisify } from 'util';
+import jwt from 'jsonwebtoken';
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+  const [, token] = authHeader.split(' ');
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
   return next();
 };
