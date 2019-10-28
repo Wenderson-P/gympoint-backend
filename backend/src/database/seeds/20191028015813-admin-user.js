ee4f782d5b671353bbefe79daa16f85573bc0137
Wenderson <wps713@gmail.com>
2019-10-27 23:07:48 -0300

    Adicionando bcrypt

--- backend/src/database/seeds/20191028015813-admin-user.js
+++ backend/src/database/seeds/20191028015813-admin-user.js
@@ -1,10 +1,12 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = {
   up: QueryInterface => {
     return QueryInterface.bulkInsert('users', [
       {
         name: 'Admnistrator',
         email: 'admin@gympoint.com.br',
-        password_hash: '12345678',
+        password_hash: bcrypt.hashSync('admin', 8),
         created_at: new Date(),
         updated_at: new Date(),
       },
