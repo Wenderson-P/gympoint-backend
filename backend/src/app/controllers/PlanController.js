c80fab5826ec891e89c275a70df6e834524430f3
Wenderson <wps713@gmail.com>
2019-11-14 11:47:32 -0300

    Add update method in plan controller

--- backend/src/app/controllers/PlanController.js
+++ backend/src/app/controllers/PlanController.js
@@ -35,6 +35,35 @@ class PlanController {
       price,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+    const { id } = req.body;
+
+    const plan = await Plan.findByPk(id);
+
+    if (!plan) {
+      return res.status(400).json("This plan doesn't exists");
+    }
+    const { title, duration, price } = await Plan.update(req.body, {
+      where: { id },
+    });
+
+    return res.json({
+      id,
+      title,
+      duration,
+      price,
+    });
+  }
 }
 
 export default new PlanController();
