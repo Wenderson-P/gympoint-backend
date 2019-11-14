5b29e82741718d6ff4c662153047e5a9e77301f7
Wenderson <wps713@gmail.com>
2019-11-14 11:28:52 -0300

    Add store method in plan controller
    First we check if the request is valid through yup, validating the title, duration and price
    Then we check if the plan already exists, searching by the name
    If everything is okay, we save the new plan

--- backend/src/app/controllers/PlanController.js
+++ backend/src/app/controllers/PlanController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import Plan from '../models/Plan';
 
 class PlanController {
@@ -6,6 +7,33 @@ class PlanController {
       attributes: ['title', 'duration', 'price'],
     });
     return res.json(plans);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const planExists = await Plan.findOne({
+      where: { title: req.body.title },
+    });
+
+    if (planExists) {
+      return res.status(400).json({ error: 'Plan already exist' });
+    }
+    const { title, duration, price } = await Plan.create(req.body);
+
+    return res.json({
+      title,
+      duration,
+      price,
+    });
   }
 }
 
