3a05ff9bb260d510b9f6548d17ef96418fe87332
Wenderson <wps713@gmail.com>
2019-11-14 19:54:59 -0300

    Add Enrollment migration

---
+++ backend/src/database/migrations/20191114205014-create-enrollment.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('enrollments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'students', key: 'id' },
+      },
+      plan_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'plans', key: 'id' },
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('enrollments');
+  },
+};
