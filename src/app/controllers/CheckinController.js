1ed043d03ad48b4c2d1c51c091a4399d58da08b1
Wenderson <wps713@gmail.com>
2019-12-01 10:38:44 -0300

    Add checkin in database, checking the conditions
    We check if the user has made all the allowed checkins of the week, if he has already made one checkin today.

--- src/app/controllers/CheckinController.js
+++ src/app/controllers/CheckinController.js
@@ -1,3 +1,4 @@
+import { isAfter, startOfWeek, isToday } from 'date-fns';
 import Checkin from '../models/Checkin';
 import Student from '../models/Student';
 
@@ -24,8 +25,43 @@ class CheckinController {
       return res.status(400).json({ error: "Student don't exists" });
     }
 
+    const today = new Date();
+
+    const startOfTheWeek = startOfWeek(today);
+
+    const studentCheckins = await Checkin.findAll({
+      where: { student_id: id },
+    });
+
+    let checkinsMade = 0;
+    let checkinToday = false;
+
+    studentCheckins.forEach(checkin => {
+      if (isToday(checkin.createdAt)) {
+        checkinToday = true;
+      }
+      if (isAfter(checkin.createdAt, startOfTheWeek)) {
+        checkinsMade += 1;
+      }
+    });
+
+    if (checkinsMade >= 5) {
+      return res.status(400).json({
+        error:
+          'The student has already done all the allowed check-in of the week',
+      });
+    }
+
+    if (checkinToday) {
+      return res
+        .status(400)
+        .json({ error: 'The student has already made one checkin today' });
+    }
+
+    const checkin = Checkin.create({ student_id: id });
+
     return res.json({
-      studentExists,
+      checkin,
     });
   }
 }
