27f180047743c021cdb12f1c4233e0fa75d9f164
Wenderson <wps713@gmail.com>
2019-12-10 12:06:31 -0300

    Sending email to student when a question is answered

--- src/app/controllers/AnswerHelpOrderController.js
+++ src/app/controllers/AnswerHelpOrderController.js
@@ -1,5 +1,8 @@
 import * as Yup from 'yup';
 import HelpOrder from '../models/HelpOrder';
+import Student from '../models/Student';
+import Queue from '../../lib/Queue';
+import QuestionAnsweredEmail from '../jobs/QuestionAnsweredEmail';
 
 class AnswerHelpOrderController {
   async store(req, res) {
@@ -15,15 +18,24 @@ class AnswerHelpOrderController {
     if (!(await schema.isValid(req.body))) {
       return res.status(401).json({ error: 'Validation fails' });
     }
-    const helpOrderExists = await HelpOrder.findOne({
+    const helpOrder = await HelpOrder.findOne({
       where: { id },
     });
 
-    if (!helpOrderExists) {
+    if (!helpOrder) {
       return res.status(400).json({ error: 'Help Order does not exists' });
     }
 
-    const helpOrder = await HelpOrder.update(
+    const student = await Student.findOne({
+      where: { id: helpOrder.student_id },
+    });
+
+    if (!student) {
+      return res.status(400).json({ error: 'Student does not exists' });
+    }
+    await Queue.add(QuestionAnsweredEmail.key, { student, helpOrder, answer });
+
+    await HelpOrder.update(
       {
         answer,
         answer_at: new Date(),
