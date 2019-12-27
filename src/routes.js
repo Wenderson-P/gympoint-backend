33da1b947314a781baa15c6ac8c02b694699bfa7
Wenderson <wps713@gmail.com>
2019-12-27 17:37:37 -0300

    Add route to student delete method

--- src/routes.js
+++ src/routes.js
@@ -23,6 +23,7 @@ routes.use(authMiddleware);
 routes.get('/students', StudentController.index);
 routes.post('/students', StudentController.store);
 routes.put('/students', StudentController.update);
+routes.delete('/students/:id', StudentController.delete);
 
 routes.get('/plans', PlanController.index);
 routes.post('/plans', PlanController.store);
