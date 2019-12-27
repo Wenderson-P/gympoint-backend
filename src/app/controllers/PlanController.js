cc149d9c8d68f55289a826d1e843ef8474485c6f
Wenderson <wps713@gmail.com>
2019-12-27 17:38:14 -0300

    Add id field in return to plan index query

--- src/app/controllers/PlanController.js
+++ src/app/controllers/PlanController.js
@@ -5,7 +5,7 @@ import Plan from '../models/Plan';
 class PlanController {
   async index(req, res) {
     const plans = await Plan.findAll({
-      attributes: ['title', 'duration', 'price'],
+      attributes: ['id', 'title', 'duration', 'price'],
     });
     return res.json(plans);
   }
